24beb478aef75cb22dbc5676a0f21ce21b4cf801
AsaneKento <74521188+AsaneKento@users.noreply.github.com>
2023-09-07 00:46:44 +0900

    :wastebasket: TailwindCSSを削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { ReactElement } from "react"
-import { Layout } from "~/components/Layout/Layout"
 import type { Metadata } from "next"
 import "./globals.css"
 
@@ -15,9 +14,7 @@ export default function RootLayout({
 }): ReactElement {
   return (
     <html lang={"ja"}>
-      <body className={"m-auto max-w-7xl"}>
-        <Layout>{children}</Layout>
-      </body>
+      <body className={"m-auto max-w-7xl"}>{children}</body>
     </html>
   )
 }
