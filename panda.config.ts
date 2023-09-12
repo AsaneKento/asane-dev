5f748c2fdc673bc08626c420e8141898bc334165
AsaneKento <74521188+AsaneKento@users.noreply.github.com>
2023-09-13 03:19:50 +0900

    :sparkles: 各種設定の追加

--- panda.config.ts
+++ panda.config.ts
@@ -1,22 +1,77 @@
 import { defineConfig } from "@pandacss/dev"
 
 export default defineConfig({
-    // Whether to use css reset
-    preflight: true,
-
-    // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}"],
-
-    // Files to exclude
-    exclude: [],
-
-    // Useful for theme customization
-    theme: {
-      extend: {}
+  jsxFramework: "react",
+  preflight: true,
+  include: ["./src/**/*.{js,jsx,ts,tsx}"],
+  exclude: [],
+  theme: {
+    extend: {
+      tokens: {
+        colors: {
+          primary: {
+            value: "#F91880",
+          },
+          secondary: {
+            value: "#3b82f6",
+          },
+          accent: {
+            value: "",
+          },
+          info: {
+            value: "#0284c7",
+          },
+          success: {
+            value: "#16a34a",
+          },
+          warning: {
+            value: "#991b1b",
+          },
+          error: {
+            value: "#b91c1c",
+          },
+          white: {
+            value: "#F1F5F9",
+            description: "メインフォントカラー",
+          },
+          black: {
+            value: "#030712",
+            description: "サブフォントカラー",
+          },
+          base: {
+            value: "#0F1729",
+            description: "背景などのメインベースカラー",
+          },
+          base100: {
+            value: "#404040",
+            description: "コンテンツカラー → 暗め",
+          },
+          base200: {
+            value: "#525252",
+            description: "コンテンツカラー → 普通",
+          },
+          base300: {
+            value: "#737373",
+            description: "コンテンツカラー → 明るめ",
+          },
+        },
+        zIndex: {
+          drawer: { value: 1200 },
+          modal: { value: 1300 },
+          snackbar: { value: 1400 },
+        },
+      },
     },
-
-    // The output directory for your css system
-    outdir: "./src/styled-system",
-
-
+  },
+  globalCss: {
+    html: {
+      h: 'full',
+    },
+    body: {
+      h: 'full',
+      bgColor: 'base',
+      color: 'white'
+    },
+  },
+  outdir: "./src/styled-system",
 })
