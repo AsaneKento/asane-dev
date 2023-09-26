d28502763b9979d254113d352179dbe5b4ecabf7
AsaneKento <74521188+AsaneKento@users.noreply.github.com>
2023-09-27 01:50:24 +0900

    :recycle: Buttonコンポーネントのcssを上書きできるようにする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import type { ReactElement } from "react"
-import { Button } from "~/libs/Button/Button"
+import { Button } from "~/libs/Button"
 import { css } from "~/styled-system/css"
 
 export default function Home(): ReactElement {
   return (
     <>
       <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>Hello</div>
-      <Button variant={"contained"} size={"medium"} color={"secondary"}>
+      <Button
+        variant={"contained"}
+        size={"medium"}
+        color={"secondary"}
+        css={{ m: "5", px: "12", py: "12", bgColor: "green.400" }}
+      >
         Sample
       </Button>
     </>
