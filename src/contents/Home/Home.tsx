043cef1394762290bd695e0eb142e973399ca9b6
AsaneKento <74521188+AsaneKento@users.noreply.github.com>
2025-02-26 02:42:32 +0900

    style: 背景色を調整した

--- src/contents/Home/Home.tsx
+++ src/contents/Home/Home.tsx
@@ -2,7 +2,7 @@ import type { ReactElement } from "react"
 
 export function Home(): ReactElement {
   return (
-    <main>
+    <main className={"min-h-screen bg-background py-16 px-4 sm:px-6 lg:px-8"}>
       <p>Hello World</p>
     </main>
   )
